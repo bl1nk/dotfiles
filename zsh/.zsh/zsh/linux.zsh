141355294be1c72eca3cb1794b73537eac146527
Markus Cisler <m@kuchen.io>
2014-03-16 12:38:01 +0100

    fix terminal beeping (which doesn't work on osx)

--- zsh/.zsh/zsh/linux.zsh
+++ zsh/.zsh/zsh/linux.zsh
@@ -5,19 +5,19 @@ if [[ -x `which systemctl` ]]; then
 	alias sc='s systemctl '
 fi
 
-# beep after commands
-bellchar=$'\a'
-setterm -blength 0 # Don't REALLY beep
-zle-line-init () { echo -n "$bellchar" }
-zle -N zle-line-init
-
 # colors in framebuffer
 if [[ $TERM = "linux" ]]; then
 	${HOME}/bin/parse_xdefaults.sh
 	clear #for background artifacting
 fi
 
-# some better colors for ls
 if [[ "`uname -s`" == "Linux" ]]; then
+	# some better colors for ls
 	eval "`dircolors ~/.dircolors`"
+
+	# beep after commands
+	bellchar=$'\a'
+	setterm -blength 0 # Don't REALLY beep
+	zle-line-init () { echo -n "$bellchar" }
+	zle -N zle-line-init
 fi
