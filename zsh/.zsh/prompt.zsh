a6e1f0e8eef43feda2be1f63ccb59e5d69acbec1
Markus Cisler <m@kuchen.io>
2015-03-16 01:46:33 +0100

    zsh: change prompt

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -1,28 +1,9 @@
 # prompt
 setopt prompt_subst
-
-# colored path
-function p_colored_path {
-    local slash="%F{yellow}/%f"
-    echo "${${PWD/#$HOME/~}//\//$slash}"
+function remote_prompt {
+	[[ -n $SSH_CLIENT ]] && echo -n " %F{white}%m%f"
 }
 
-# git info
-function p_vcs {
-    vcs_info
-    echo $vcs_info_msg_0_
-}
-
-# environments:
-#  - ssh
-#  - virtualenv
-export VIRTUAL_ENV_DISABLE_PROMPT=1
-function p_envs {
-    local envs
-    [[ -n $SSH_CLIENT ]]  && envs+="×"
-    [[ -n $VIRTUAL_ENV ]] && envs+="P"
-
-    [[ -n $envs ]] && echo " %F{red}$envs%f"
-}
-
-PROMPT='%f$(p_colored_path)$(p_vcs)$(p_envs) %F{green}λ%f '
+PROMPT='
+%F{green}%5~%f$(remote_prompt)
+%(!.%F{red}.%F{white})λ%f '
