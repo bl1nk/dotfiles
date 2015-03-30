31e0d259e0a878e3dcef0e36a8660e429bd46547
Markus Cisler <mrkscslr@gmail.com>
2015-03-31 00:42:23 +0200

    zsh: prompt colors, vim is nvim now

--- zsh/.zsh/osx.zsh
+++ zsh/.zsh/osx.zsh
@@ -1,22 +1,19 @@
 if [[ "$OSTYPE" == darwin* ]]; then
-	export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
+    export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
 
-	export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH
-	export PATH=$PATH:/usr/local/opt/ruby/bin
-	export PATH=$PATH:/usr/local/Cellar/go/1.2.1/libexec/bin
-	export GOPATH=$HOME/go
-	export PATH=$PATH:$GOPATH/bin
-	export PATH=$PATH:/usr/local/opt/go/libexec/bin
-	. $HOME/perl5/perlbrew/etc/bashrc
+    export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH
+    export PATH=$PATH:/usr/local/opt/ruby/bin
 
-	export LC_ALL="en_US.UTF-8"
+    export LC_ALL="en_US.UTF-8"
+    export EDITOR="nvim"
+    export VISUAL="nvim"
 
-	alias ls="ls -Gsh"
-	alias lt="ls -tcr"
-	alias llt="l -tcr"
-	alias q="qlmanage -p $@ >/dev/null 2>&1"
-	alias mosh="LANG=en_US.UTF-8 mosh"
-	alias vim="nvim"
+    alias ls="ls -Gsh"
+    alias lt="ls -tcr"
+    alias llt="l -tcr"
+    alias q="qlmanage -p $@ >/dev/null 2>&1"
+    alias mosh="LANG=en_US.UTF-8 mosh"
+    alias vim="nvim"
 
-	twitch() { livestreamer --player 'mpv --cache 256' http://www.twitch.tv/$1 source; }
+    twitch() { livestreamer --player 'mpv --cache 256' http://www.twitch.tv/$1 source; }
 fi
--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -1,9 +1,6 @@
 # prompt
 setopt prompt_subst
-function remote_prompt {
-	[[ -n $SSH_CLIENT ]] && echo -n " %F{white}%m%f"
-}
 
 PROMPT='
-%{%F{magenta}%}%n%{%f%} @ %{%F{yellow}%}%m%{%f%} : %{%F{green}%}%5~%{%f%}
+%{%F{magenta}%}%n%{%f%F{white}%} @ %{%f%F{yellow}%}%m%{%f%F{white}%} : %{%f%F{green}%}%6~%{%f%}
 %{%(!.%F{red}.%F{white})%}─╼%{%f%} '
