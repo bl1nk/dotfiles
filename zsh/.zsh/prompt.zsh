a267da8878cda0fca42d3d15e51ed2e5cfed1da1
Markus Cisler <mrkscslr@gmail.com>
2015-03-31 01:16:13 +0200

    zsh: fuck box-drawing chars

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -3,4 +3,4 @@ setopt prompt_subst
 
 PROMPT='
 %{%F{magenta}%}%n%{%f%F{white}%} @ %{%f%F{yellow}%}%m%{%f%F{white}%} : %{%f%F{green}%}%6~%{%f%}
-%{%(!.%F{red}.%F{white})%}╼%{%f%} '
+%{%(!.%F{red}%}#%{.%F{white}%}$%{)%f%} '
