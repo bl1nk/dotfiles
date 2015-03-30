a4ceb989f95f643a749c8336c11caa537d63bc4f
Markus Cisler <mrkscslr@gmail.com>
2015-03-31 01:06:11 +0200

    aaaaa

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -3,4 +3,4 @@ setopt prompt_subst
 
 PROMPT='
 %{%F{magenta}%}%n%{%f%F{white}%} @ %{%f%F{yellow}%}%m%{%f%F{white}%} : %{%f%F{green}%}%6~%{%f%}
-%{%(!.%F{red}.%F{white})%}─╼%{%f%} '
+%{%(!.%F{red}.%F{white})%}╼%{%f%} '
