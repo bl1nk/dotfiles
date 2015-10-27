0a33e8695cde2322644e3e3cf57060ef47cc9a39
Markus Cisler <hello@markuscisler.com>
2015-10-27 08:07:01 +0100

    finicky: add .finicky.js

---
+++ misc/.finicky.js
@@ -0,0 +1,17 @@
+finicky.setDefaultBrowser('com.apple.safari')
+// Open Spotify links in client
+finicky.onUrl(function(url, opts) {
+    if (url.match(/^https?:\/\/open\.spotify\.com/)) {
+        return {
+            bundleIdentifier: 'com.spotify.client'
+        }
+    }
+});
+// By supplying an array of bundle identifiers, finicky opens the url in the first one
+// that's currently running. If none are running, the first app in the array is started.
+finicky.onUrl(function(url, opts) {
+    return {
+        bundleIdentifier: ['com.apple.Safari', 'com.google.Chrome','org.mozilla.firefox']
+    }
+});
+
