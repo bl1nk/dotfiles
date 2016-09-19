0405d8d1dace2c397cd3c371a298d37fbe547600
Markus Cisler <hello@markuscisler.com>
2016-09-19 14:11:50 +0200

    cya nvim, remove unneeded aliases

--- zsh/.zsh/osx.zsh
+++ zsh/.zsh/osx.zsh
@@ -5,8 +5,8 @@ if [[ "$OSTYPE" == darwin* ]]; then
     export PATH=$PATH:/usr/local/opt/ruby/bin
 
     export LC_ALL="en_US.UTF-8"
-    export EDITOR="nvim"
-    export VISUAL="nvim"
+    export EDITOR="vim"
+    export VISUAL="vim"
     export HOMEBREW_NO_ANALYTICS=1
 
     alias ls="ls -Gsh"
@@ -14,24 +14,19 @@ if [[ "$OSTYPE" == darwin* ]]; then
     alias llt="l -tcr"
     alias q="qlmanage -p $@ >/dev/null 2>&1"
     alias mosh="LANG=en_US.UTF-8 mosh"
-    alias vim="nvim"
-    alias bubu="brew update && brew upgrade"
 
     twitch() { livestreamer --player 'mpv --cache 256' http://www.twitch.tv/$1 source; }
 fi
 
 if [[ "$HOST" == myt-* ]]; then
-    export PATH=$PATH:$HOME/Git/work/ssh-scripts
-    #if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
-    #export PYENV_ROOT=/usr/local/var/pyenv
-    export NVIM_TUI_ENABLE_TRUE_COLOR=1
+    export PATH=$PATH:$HOME/git-work/ssh-scripts
 
     alias sp="speedtest-cli --share --simple"
     alias fwfuck="sudo pfctl -d"
     pv() { source $HOME/.venv/$1/bin/activate ; }
-    alias dup="docker-machine start"
-    alias ddown="docker-machine stop"
 
     export GOPATH=$HOME/go
     export PATH=$PATH:$GOPATH/bin
+
+    export HOMEBREW_CASK_OPTS="--caskroom=/opt/homebrew-cask/Caskroom"
 fi
