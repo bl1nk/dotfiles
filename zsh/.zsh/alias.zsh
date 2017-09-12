9a6c5f8b28509f8fdb508ce324f066e8c3a08c5e
Markus Cisler <hello@markuscisler.com>
2017-09-12 14:21:20 +0200

    zsh: use better tm function instead of alias

--- zsh/.zsh/alias.zsh
+++ zsh/.zsh/alias.zsh
@@ -23,4 +23,10 @@ alias mkdir='nocorrect mkdir'
 alias wget='nocorrect noglob wget'
 alias wgetc="wget --content-disposition"
 
-alias tm="if tmux has; then tmux attach -d; else tmux new; fi"
+tm() {
+    local session
+    newsession=${1:-new}
+    session=$(tmux list-sessions -F "#{session_name}" | \
+        fzf --query="$1" --select-1 --exit-0) &&
+        tmux attach-session -d -t "$session" || tmux new-session -s $newsession
+}
