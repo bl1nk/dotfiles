42d558d832178e1701bf62055cb47c7129f3f409
Markus Cisler <hello@markuscisler.com>
2017-12-20 10:50:11 +0100

    zsh: Use pure prompt

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -1,45 +1,4 @@
 # prompt
-setopt prompt_subst
+autoload -U promptinit; promptinit
 
-# old prompt
-#PROMPT='
-#%{%F{blue}%}%n%{%f%F{white}%} at %{%f%F{yellow}%}%m%{%f%F{white}%} in %{%f%F{magenta}%}%6~%{%f%}
-#%{%(!.%F{red}%}#%{.%F{white}%}$%{)%f%} '
-
-# gitpwd - print %~, limited to $NDIR segments, with inline git branch
-NDIRS=2
-gitpwd() {
-  local -a segs splitprefix; local prefix branch
-  segs=("${(Oas:/:)${(D)PWD}}")
-  segs=("${(@)segs/(#b)(?(#c10))??*(?(#c5))/${(j:\u2026:)match}}")
-
-  if gitprefix=$(git rev-parse --show-prefix 2>/dev/null); then
-    splitprefix=("${(s:/:)gitprefix}")
-    if ! branch=$(git symbolic-ref -q --short HEAD); then
-      branch=$(git name-rev --name-only HEAD 2>/dev/null)
-      [[ $branch = *\~* ]] || branch+="~0"    # distinguish detached HEAD
-    fi
-    if (( $#splitprefix > NDIRS )); then
-      print -n "${segs[$#splitprefix]}@$branch "
-    else
-      segs[$#splitprefix]+=@$branch
-    fi
-  fi
-
-  (( $#segs == NDIRS+1 )) && [[ $segs[-1] == "" ]] && print -n /
-  print "${(j:/:)${(@Oa)segs[1,NDIRS]}}"
-}
-
-cnprompt6() {
-  case "$TERM" in
-    xterm*|rxvt*)
-      precmd() { [[ -t 1 ]] && print -Pn "\e]0;%m: %~\a" }
-      preexec() { [[ -t 1 ]] && print -n "\e]0;$HOST: ${(q)1//(#m)[$'\000-\037\177-']/${(q)MATCH}}\a" }
-  esac
-  setopt PROMPT_SUBST
-  nbsp=$'\u00A0'
-  PS1='%B%m${TENV:+ [$TENV]}%(?.. %??)%(1j. %j&.)%b $(gitpwd)%B%(!.%F{red}.%F{yellow})%#${SSH_CONNECTION:+%#}$nbsp%b%f'
-  RPROMPT=''
-}
-
-cnprompt6
+prompt pure
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -37,6 +37,8 @@ export LESS_TERMCAP_se=$'\E[0m'
 export LESS_TERMCAP_ue=$'\E[0m'
 export LESS_TERMCAP_us=$'\E[00;32m'
 
+fpath=( "$HOME/.zsh/fpath" $fpath )
+
 # configuration files
 for file in ~/.zsh/*.zsh; do
 	source $file
