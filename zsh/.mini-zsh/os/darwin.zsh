061abc4064f655b3ea2f7b46f35995c5af7e8c26
Markus Cisler <hello@markuscisler.com>
2018-04-12 10:35:21 +0200

    zsh: fix mosh alias that was not broken before and I don't know

--- zsh/.mini-zsh/os/darwin.zsh
+++ zsh/.mini-zsh/os/darwin.zsh
@@ -2,5 +2,5 @@ export HOMEBREW_NO_ANALYTICS=1
 export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
 export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH
 
-alias mosh="LANG=en_US.UTF-8 mosh"
+alias mosh="LC_CTYPE=en_US.UTF-8 LC_MESSAGES=en_US.UTF-8 mosh"
 alias ls='ls -Gsh'
