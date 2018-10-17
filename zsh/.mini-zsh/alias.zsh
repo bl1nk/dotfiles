6b384afb9e18b3946d86915be61d4349e7532031
Markus Cisler <hello@markuscisler.com>
2018-10-17 16:19:27 +0200

    zsh: update aliases

--- zsh/.mini-zsh/alias.zsh
+++ zsh/.mini-zsh/alias.zsh
@@ -1,9 +1,9 @@
 alias ls='ls -sh --color=auto'
-alias ll='ls -l'
-alias l='ll'
-alias lt='ll -tr'
+alias ll='ls -lF'
 alias la='ls -a'
 alias lla='ll -a'
+
+alias sudo='sudo '
 
 alias tm="tmux attach -d || tmux"
 alias td='tmux detach'
@@ -19,6 +19,7 @@ alias gs='git status'
 alias gl='git log --all --decorate --oneline --graph'
 alias glp='git log --all --decorate --oneline --graph -p'
 alias gla='git lg --all'
+alias gpr='git pull --rebase'
 
 alias mkdir='nocorrect mkdir'
 alias wget='nocorrect noglob wget'
