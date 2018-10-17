f540d8d1592ac1115681a3cbc296eca371469f0c
Markus Cisler <hello@markuscisler.com>
2018-10-17 16:21:50 +0200

    zsh: fjksdalfjsdkalf

--- zsh/.mini-zsh/plugins/colorful_pager.zsh
+++
@@ -1,7 +0,0 @@
-# colorful $PAGER
-export LESS_TERMCAP_mb=$'\E[01;31m'
-export LESS_TERMCAP_md=$'\E[01;31m'
-export LESS_TERMCAP_me=$'\E[0m'
-export LESS_TERMCAP_se=$'\E[0m'
-export LESS_TERMCAP_ue=$'\E[0m'
-export LESS_TERMCAP_us=$'\E[00;32m'
--- zsh/.zshrc
+++ zsh/.zshrc
@@ -1,2 +1,3 @@
 MINI_ZSH_CONF="${HOME}/.mini-zsh/zsh.conf"
 [[ -r "${MINI_ZSH_CONF}" ]] && . "${MINI_ZSH_CONF}"
+
