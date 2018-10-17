cdaaedfcf3fe85a02fc02ffc27b4eb8132741268
Markus Cisler <hello@markuscisler.com>
2018-10-17 16:21:03 +0200

    zsh: restructure darwin-specific stuff

--- zsh/.mini-zsh/os/darwin.zsh
+++ zsh/.mini-zsh/os/darwin.zsh
@@ -1,8 +1,15 @@
 #!/usr/bin/env zsh
 
 export HOMEBREW_NO_ANALYTICS=1
-export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
-export PATH=$HOME/bin:/usr/local/bin:/usr/local/sbin:$PATH
+
+export PATH="/usr/local/bin:$PATH"
+export PATH="/usr/local/sbin:$PATH"
+
+# gnu coreutils
+export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
 
 alias mosh="LC_CTYPE=en_US.UTF-8 LC_MESSAGES=en_US.UTF-8 mosh"
+
+# correct colors for ls
+export LSCOLORS=gxfxbEaEBxxEhEhBaDaCaD
 alias ls='ls -Gsh'
