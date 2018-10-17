2827f723cadb86ea23b0fcde89868670302734b1
Markus Cisler <hello@markuscisler.com>
2018-10-17 16:22:27 +0200

    zsh: add ruby

---
+++ zsh/.mini-zsh/plugins/ruby.zsh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+if (( $+commands[chruby] ))
+then
+    [ -f /usr/local/share/chruby/chruby.sh ] && source /usr/local/share/chruby/chruby.sh
+    [ -f /usr/local/opt/chruby/share/chruby/auto.sh ] && source /usr/local/opt/chruby/share/chruby/auto.sh
+fi
