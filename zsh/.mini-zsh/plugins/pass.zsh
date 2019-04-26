af6ea645fd62f80a336043f56a38fe78656adf58
Markus Cisler <hello@markuscisler.com>
2019-04-26 13:18:17 +0200

    zsh: make homebrew pass-otp work

--- zsh/.mini-zsh/plugins/pass.zsh
+++ zsh/.mini-zsh/plugins/pass.zsh
@@ -2,5 +2,11 @@
 if (( $+commands[pass] ))
 then
     alias ppr='pass git pull --rebase'
+
+    EXTENSIONS_DIR=/usr/local/lib/password-store/extensions
+    if [ -d "${EXTENSIONS_DIR}" ]; then
+	    export PASSWORD_STORE_ENABLE_EXTENSIONS=true
+	    export PASSWORD_STORE_EXTENSIONS_DIR=${EXTENSIONS_DIR}
+    fi
 fi
 
