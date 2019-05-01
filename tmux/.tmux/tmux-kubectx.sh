bbf76216d65c81fa3f13643a5ebf9b6d1e23d591
Markus Cisler <hello@markuscisler.com>
2019-05-01 14:12:27 +0200

    tmux: shows kubectx if not 'default' or 'minikube'

---
+++ tmux/.tmux/tmux-kubectx.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if command -v kubectl &>/dev/null; then
+    currentContext=$(kubectl config current-context)
+    if [ "${currentContext}" = "default" ] || [ "${currentContext}" = "minikube" ]; then
+        echo " ${currentContext} "
+    fi
+fi
