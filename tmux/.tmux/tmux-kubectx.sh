b4ad12a4c2a0933776dc0774a68800450e63b69f
Markus Cisler <hello@markuscisler.com>
2019-05-01 14:55:37 +0200

    tmux: only print kubectx if not default or minikube

--- tmux/.tmux/tmux-kubectx.sh
+++ tmux/.tmux/tmux-kubectx.sh
@@ -1,8 +1,7 @@
 #!/usr/bin/env bash
-
 if command -v kubectl &>/dev/null; then
     currentContext=$(kubectl config current-context)
-    if [ "${currentContext}" = "default" ] || [ "${currentContext}" = "minikube" ]; then
+    if [ "${currentContext}" != "default" ] && [ "${currentContext}" != "minikube" ]; then
         echo " ${currentContext} "
     fi
 fi
