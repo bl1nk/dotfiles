afe7886b501bf6a1e164397c51b6139eec32d03b
Markus Cisler <hello@markuscisler.com>
2019-10-10 20:07:05 +0200

    zsh: use vi keybinds

--- zsh/.mini-zsh/plugins/binds.zsh
+++ zsh/.mini-zsh/plugins/binds.zsh
@@ -1,6 +1,8 @@
 #!/usr/bin/env zsh
 
-bindkey -e
+export KEYTIMEOUT=1
+
+bindkey -v
 bindkey '\e[1~' beginning-of-line
 bindkey '\e[4~' end-of-line
 bindkey '\e[3~' delete-char
@@ -9,3 +11,23 @@ bindkey "^[[7~" beginning-of-line # Pos1
 bindkey "^[[8~" end-of-line # End
 bindkey "^[[A" history-beginning-search-backward
 bindkey "^[[B" history-beginning-search-forward
+
+autoload edit-command-line
+zle -N edit-command-line
+bindkey -M 'emacs' '\ev' edit-command-line
+bindkey -M 'vicmd' '\ev' edit-command-line
+
+bindkey '^P' up-history
+bindkey '^N' down-history
+
+# backspace and ^h working even after
+# returning from command mode
+bindkey '^?' backward-delete-char
+bindkey '^h' backward-delete-char
+
+# ctrl-w removed word backwards
+bindkey '^w' backward-kill-word
+
+# ctrl-r starts searching history backward
+bindkey '^r' history-incremental-search-backward
+
