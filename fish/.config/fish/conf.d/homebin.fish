33ef037e1a2decf49bac9d76e391669f9c63a6f8
Markus Cisler <hello@markuscisler.com>
2019-10-15 12:11:31 +0200

    fish: add /usr/local/bin to fish_user_paths

--- fish/.config/fish/conf.d/homebin.fish
+++ fish/.config/fish/conf.d/homebin.fish
@@ -1,3 +1,6 @@
 if test -d $HOME/.local/bin
     set -g fish_user_paths "$HOME/.local/bin" $fish_user_paths
 end
+if test -d /usr/local/bin
+	set -g fish_user_paths /usr/local/bin $fish_user_paths
+end
